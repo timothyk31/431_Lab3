c0aa978177a828dbc4e6efde8acbc544d2e208de
root <root@localhost>
2025-01-28 07:25:33 +0000

    added tests and attributes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,7 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title)
+      params.require(:book).permit(:title, :author, :price, :published_date)
+
     end
 end
