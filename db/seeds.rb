425661e8a0ef67b70ab44251928b9f78fae753b0
timothyk31 <timothykurniawan16@gmail.com>
2025-02-04 02:42:25 +0000

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create!([
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        price: 12.49,
+        published_date: "1925-04-10"
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        price: 9.99,
+        published_date: "1960-07-11"
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        price: 8.99,
+        published_date: "1949-06-08"
+    },
+    {
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+        price: 7.99,
+        published_date: "1813-01-28"
+    },
+    {
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        price: 6.99,
+        published_date: "1951-07-16"
+    }])
